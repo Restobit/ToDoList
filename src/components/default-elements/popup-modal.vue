4a50cb91a3112cabc7e42c8a8aacff88473441e0
Fekete Ottó <feketeotto@Mac-MacBook-Pro.local>
2021-03-04 06:59:08 +0100

    init

---
+++ src/components/default-elements/popup-modal.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot></slot>
+    <button @click="close">Close Modal</button>
+  </div>
+</template>
+
+<script>
+export default{
+  methods:{
+    close(){
+      this.$emit('close');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
